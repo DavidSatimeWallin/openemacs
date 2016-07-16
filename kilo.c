f474514a6cc38edd5d506bf3f1a509bbc5cd1a06
practicalswift <practicalswift@users.noreply.github.com>
2016-07-16 19:28:35 +0200

    Change color mapping.

--- kilo.c
+++ kilo.c
@@ -482,13 +482,13 @@ void editor_update_syntax(editor_row *row) {
 int editor_syntax_to_color(int hl) {
     switch (hl) {
     case HL_COMMENT:
-    case HL_MLCOMMENT: return 31;   /* red */
-    case HL_KEYWORD1: return 33;    /* yellow */
-    case HL_KEYWORD2: return 32;    /* green */
-    case HL_STRING: return 35;      /* magenta */
-    case HL_NUMBER: return 36;      /* cyan */
-    case HL_MATCH: return 34;       /* blue */
-    default: return 37;             /* white */
+    case HL_MLCOMMENT: return 31;   /* normal red */
+    case HL_KEYWORD1: return 35;    /* normal magenta */
+    case HL_KEYWORD2: return 32;    /* normal green */
+    case HL_STRING: return 95;      /* bright magenta */
+    case HL_NUMBER: return 97;      /* bright white */
+    case HL_MATCH: return 34;       /* normal blue */
+    default: return 37;             /* normal white */
     }
 }
 
