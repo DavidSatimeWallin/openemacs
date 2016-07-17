7668bb7d59401bb7e8e4f689f72cce8c739c0969
practicalswift <practicalswift@users.noreply.github.com>
2016-07-17 18:24:19 +0200

    Add ":" as a separator.

--- kilo.c
+++ kilo.c
@@ -291,7 +291,7 @@ int get_window_size(int *rows, int *columns) {
 /* ====================== Syntax highlight color scheme  ==================== */
 
 int is_separator(int c) {
-    return c == '\0' || isspace(c) || strchr(",.()+-/*=~%[];", c) != NULL;
+    return c == '\0' || isspace(c) || strchr(",.()+-/*=~%[];:", c) != NULL;
 }
 
 /* Return true if the specified row last char is part of a multi line comment
