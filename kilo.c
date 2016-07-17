33b4454342466afaff3aa2aa221e20b31031422c
practicalswift <practicalswift@users.noreply.github.com>
2016-07-17 19:42:48 +0200

    Remove dead code.

--- kilo.c
+++ kilo.c
@@ -251,26 +251,6 @@ int editor_read_key(int fd) {
         }
     }
 }
-/* Use the ESC [6n escape sequence to query the horizontal cursor position
- * and return it. On error -1 is returned, on success the position of the
- * cursor is stored at *rows and *columns and 0 is returned. */
-int get_cursor_position(int ifd, int ofd, int *rows, int *columns) {
-    /* Report cursor location */
-    if (write(ofd, "\x1b[6n", 4) != 4) return -1;
-    /* Read the response: ESC [ rows ; columns R */
-    unsigned int i = 0;
-    char buf[32];
-    while (i < sizeof(buf) - 1) {
-        if (read(ifd, buf + i, 1) != 1) break;
-        if (buf[i] == 'R') break;
-        i++;
-    }
-    buf[i] = '\0';
-    /* Parse it. */
-    if (buf[0] != ESC || buf[1] != '[') return -1;
-    if (sscanf(buf + 2, "%d;%d", rows, columns) != 2) return -1;
-    return 0;
-}
 
 /* Try to get the number of columns in the current terminal.
  * Returns 0 on success, -1 on error. */
